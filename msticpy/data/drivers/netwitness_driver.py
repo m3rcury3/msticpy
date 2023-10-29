fdd0fa4b15efa347ee2d518a2a5d78445b5e4208
m3rcury3 <m3rcury3@outlook.com>
2023-10-29 09:20:14 +0300

    dataframe display

--- msticpy/data/drivers/netwitness_driver.py
+++ msticpy/data/drivers/netwitness_driver.py
@@ -476,22 +476,23 @@ class NetwitnessAPI():
             _json_data = json.loads(response.content.decode("utf-8"))
             pd.DataFrame(_json_data)
             df = pd.DataFrame.from_dict(_json_data)
-            final_list =[] 
-            if(len(_json_data) == 3):
-                raise Exception("Query Returned Empty Results")
-            for x in _json_data:
-                final_list += x["results"]["fields"]
-            len(final_list)
-            df2 = pd.json_normalize(final_list)
-            #Added the following as pivot was causing a valueerror due to duplicate values occurring when 'pivoting'
+            # final_list =[] 
+            # if(len(_json_data) == 3):
+            #     raise Exception("Query Returned Empty Results")
+            # for x in _json_data:
+            #     final_list += x["results"]["fields"]
+            # len(final_list)
+            # df2 = pd.json_normalize(final_list)
+            # #Added the following as pivot was causing a valueerror due to duplicate values occurring when 'pivoting'
             
-            df3=df2[["group","type","value"]].drop_duplicates(subset=["group","type"]).pivot(index="group",columns=["type"])
-            ##df3=df2[["group","type","value"]].pivot(index="group",columns=["type"])
+            # df3=df2[["group","type","value"]].drop_duplicates(subset=["group","type"]).pivot(index="group",columns=["type"])
+            # #df3=df2[["group","type","value"]].pivot(index="group",columns=["type"])
             # columns = [x[1] for x in list(df3.columns)]
             # df3.columns = columns
             # df3["time"] = pd.to_datetime(df3["time"], unit="s")
 
-            return df3
+            # return df3
+            return df
 
 
         elif (nw_query_type == "raw"):
