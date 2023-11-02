40fbf15940513fec0dae14119a26fe52723f878b
m3rcury3 <m3rcury3@outlook.com>
2023-11-02 07:27:57 +0300

    test

--- tests/data/drivers/test_netwitness_driver.py
+++ tests/data/drivers/test_netwitness_driver.py
@@ -17,10 +17,7 @@ from msticpy.common.exceptions import (
     MsticpyNotConnectedError,
     MsticpyUserConfigError,
 )
-
-from msticpy.data.drivers.netwitness_driver import NetwitnessDriver, NetwitnessAPI
-
-from ...unit_test_lib import get_test_data_path
+from msticpy.data.drivers.netwitness_driver import NetwitnessDriver
 
 NETWITNESS_CLI_PATCH = NetwitnessDriver.__module__ + ".NetwitnessAPI"
 
@@ -73,7 +70,7 @@ def test_netwitness_connect_no_params(netwitness_client):
 @patch(NETWITNESS_CLI_PATCH)
 def test_netwitness_connect_req_params(netwitness_client):
     """Check load/connect success with required params."""
-    #netwitness_client.connect = cli_connect
+    netwitness_client.connect = cli_connect
 
     nw_driver = NetwitnessDriver()
     check.is_true(nw_driver.loaded)
