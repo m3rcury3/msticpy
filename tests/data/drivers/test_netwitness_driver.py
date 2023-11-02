28ac7930ac1155765ef35d9852354d9050b32306
m3rcury3 <m3rcury3@outlook.com>
2023-11-02 08:54:28 +0300

    test

--- tests/data/drivers/test_netwitness_driver.py
+++ tests/data/drivers/test_netwitness_driver.py
@@ -36,22 +36,22 @@ def test_netwitness_connect_no_params(netwitness_client):
     #     check.is_false(nw_driver.connected)
     # check.is_in("no Netwitness connection parameters", mp_ex.value.args)
 
-@patch(NETWITNESS_CLI_PATCH)
-def test_netwitness_connect_req_params(netwitness_client):
-    """Check load/connect success with required params."""
-    netwitness_client.connect = cli_connect
+# @patch(NETWITNESS_CLI_PATCH)
+# def test_netwitness_connect_req_params(netwitness_client):
+#     """Check load/connect success with required params."""
+#     netwitness_client.connect = cli_connect
 
-    nw_driver = NetwitnessDriver()
-    check.is_true(nw_driver.loaded)
+#     nw_driver = NetwitnessDriver()
+#     check.is_true(nw_driver.loaded)
 
-    # [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test code")]
-    nw_driver.connect(nwhost="netwitnesshost", nwport="50103", nwuser="testuser", nwpassword=_FAKE_STRING)  # nosec
-    check.is_true(nw_driver.connected)
+#     # [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test code")]
+#     nw_driver.connect(nwhost="netwitnesshost", nwport="50103", nwuser="testuser", nwpassword=_FAKE_STRING)  # nosec
+#     check.is_true(nw_driver.connected)
 
-    # [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test code")]
-    # nw_conn_str = (
-    #     f"nwhost='netwitnesshost'; nwuser='testuser'; nwpassword='{_FAKE_STRING}'"  # nosec
-    # )
-    # nw_driver = NetwitnessDriver()
+#     # [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test code")]
+#     # nw_conn_str = (
+#     #     f"nwhost='netwitnesshost'; nwuser='testuser'; nwpassword='{_FAKE_STRING}'"  # nosec
+#     # )
+#     # nw_driver = NetwitnessDriver()
 
-    # nw_driver.connect(connection_str=nw_conn_str)
+#     # nw_driver.connect(connection_str=nw_conn_str)
