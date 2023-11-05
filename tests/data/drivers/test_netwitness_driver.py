feda0fb863a032603c4ca5c84ba1b2fd0e6f4795
m3rcury3 <m3rcury3@outlook.com>
2023-11-05 11:11:51 +0300

    test

--- tests/data/drivers/test_netwitness_driver.py
+++ tests/data/drivers/test_netwitness_driver.py
@@ -14,7 +14,7 @@ from msticpy.common.exceptions import (
 def test_netwitness_connect_no_params():
     with pytest.raises(MsticpyUserConfigError) as mp_ex:
         netwitness=NetwitnessDriver()
-        netwitness.connect() ## No arguments supplied by user
+        netwitness.connect(url="xyz",nwuser="user",nwpassword="pass") ## No arguments supplied by user
     check.is_in("no Netwitness connection parameters", mp_ex.value.args)
 
 # def test_netwitness_connect_req_params():
