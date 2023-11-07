67d3a872e8781ee0ae264334496a0bd3b6b5a182
m3rcury3 <m3rcury3@outlook.com>
2023-11-07 13:57:43 +0300

    test

--- tests/data/drivers/test_netwitness_driver.py
+++ tests/data/drivers/test_netwitness_driver.py
@@ -26,7 +26,6 @@ def test_netwitness_connect_req_params():
     check.is_in("no Netwitness connection parameters", mp_ex.value.args)
 
 @patch('msticpy.data.drivers.netwitness_driver.NetwitnessDriver')
-@patch('connect.response')
 def test_netwitness_query_success(mock_netwitness):
     mock_netwitness=NetwitnessDriver()
     with pytest.raises(MsticpyNotConnectedError) as mp_ex:
@@ -34,7 +33,7 @@ def test_netwitness_query_success(mock_netwitness):
         check.is_false(mock_netwitness.connected)
     check.is_in("not connected to Netwitness.", mp_ex.value.args)
     mock_netwitness.connect.response = MagicMock(status_code="200")
-    check.is_true(netwitness.connected)
+    check.is_true(mock_netwitness.connected)
 
 
 #def test_netwitness_connect_no_params -- Done
