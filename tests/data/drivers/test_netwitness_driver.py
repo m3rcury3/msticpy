032315e39b482801ef71cb3ac2822219fa557899
m3rcury3 <m3rcury3@outlook.com>
2023-11-07 12:50:45 +0300

    test_netwitness_query_success

--- tests/data/drivers/test_netwitness_driver.py
+++ tests/data/drivers/test_netwitness_driver.py
@@ -8,26 +8,30 @@ import pytest
 import pytest_check as check
 from msticpy.data.drivers.netwitness_driver import NetwitnessDriver
 from msticpy.common.exceptions import (
+    MsticpyNotConnectedError,
     MsticpyUserConfigError,
-    MsticpyConnectionError,
 )
 
 def test_netwitness_connect_no_params():
+    netwitness=NetwitnessDriver()
     with pytest.raises(MsticpyUserConfigError) as mp_ex:
-        netwitness=NetwitnessDriver()
         netwitness.connect() ## No arguments provided
     check.is_in("no Netwitness connection parameters", mp_ex.value.args)
 
 def test_netwitness_connect_req_params():
+    netwitness=NetwitnessDriver()
     with pytest.raises(MsticpyUserConfigError) as mp_ex:
-        netwitness=NetwitnessDriver()
         netwitness.connect(nwhost="1.1.1.1") ## Missing parameters provided
     check.is_in("no Netwitness connection parameters", mp_ex.value.args)
 
+def test_netwitness_query_success():
+    netwitness=NetwitnessDriver()
+    with pytest.raises(MsticpyNotConnectedError) as mp_ex:
+        netwitness.query("some query")
+        check.is_false(netwitness.connected)
+    check.is_in("not connected to Netwitness.", mp_ex.value.args)
+
 
 #def test_netwitness_connect_no_params -- Done
-#def test_netwitness_connect_req_params
-#def test_netwitness_connect_errors
-#def test_netwitness_fired_alerts
-#def test_netwitness_saved_searches
+#def test_netwitness_connect_req_params -- Done
 #def test_netwitness_query_success
