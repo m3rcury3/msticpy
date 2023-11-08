eab3816edf6f9d1a96d74431b9bb3715d0296640
m3rcury3 <m3rcury3@outlook.com>
2023-11-08 12:54:13 +0300

    test

--- tests/data/drivers/test_netwitness_driver.py
+++ tests/data/drivers/test_netwitness_driver.py
@@ -30,6 +30,7 @@ def test_netwitness_connection_success(mock_netwitness_api):
     mock_netwitness_api.return_value=MagicMock(status_code="200")
     netwitness=NetwitnessDriver()
     netwitness.connect(nwhost="1.1.1.1",nwuser="username",nwpassword="pass")
+    print("!!!!!!!!!!!!!!!!!!!!!Connected Value is:" + str(netwitness.connected))
     check.is_true(netwitness.connected)
 
 
