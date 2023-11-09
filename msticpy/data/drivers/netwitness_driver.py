301c6381be32fa0d5ece05929abc7d2df42d03b7
m3rcury3 <m3rcury3@outlook.com>
2023-11-09 07:14:24 +0300

    test

--- msticpy/data/drivers/netwitness_driver.py
+++ msticpy/data/drivers/netwitness_driver.py
@@ -88,7 +88,7 @@ class NetwitnessDriver(DriverBase):
         }
         nwurl="http://" + arg_dict['nwhost'] + ":" + str(arg_dict['nwport'])
         print("Connecting to " + nwurl + " as " + arg_dict['nwuser'] + "...")
-        self.nw_client.login(url=nwurl, username=arg_dict['nwuser'], password=arg_dict['nwpassword'])
+        self.nw_client.login(url=nwurl, username=arg_dict['nwuser'], password=arg_dict['nwpassword'],**kwargs)
         print("self.nw_client.response.status_code = " + str(self.nw_client.response.status_code))
         print("self.nw_client.response.headers = " + str(self.nw_client.response.headers))
         print("self.nw_client.response.reason = " + str(self.nw_client.response.reason))
