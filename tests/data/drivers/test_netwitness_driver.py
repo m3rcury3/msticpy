3cb47329d49a26754381758f4b9f700a42e5309e
m3rcury3 <m3rcury3@outlook.com>
2023-11-09 11:55:49 +0300

    test

--- tests/data/drivers/test_netwitness_driver.py
+++ tests/data/drivers/test_netwitness_driver.py
@@ -3,38 +3,56 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-
+"""datq query test class."""
+import io
 from unittest.mock import MagicMock, patch
+
 import pytest
 import pytest_check as check
-from msticpy.data.drivers.netwitness_driver import NetwitnessDriver, NetwitnessAPI
+
 from msticpy.common.exceptions import (
+    MsticpyConnectionError,
+    MsticpyDataQueryError,
     MsticpyNotConnectedError,
     MsticpyUserConfigError,
 )
+from msticpy.data.drivers.netwitness_driver import NetwitnessDriver, NetwitnessAPI
 
-def test_netwitness_connect_no_params():
-    netwitness=NetwitnessDriver()
+from ...unit_test_lib import get_test_data_path
+
+_TEST_DATA = get_test_data_path()
+
+NETWITNESS_CLI_PATCH = NetwitnessDriver.__module__ + ".NetwitnessAPI"
+
+def cli_connect(**kwargs):
+    cause = MagicMock()
+    cause.body = bytes("Test body stuff", encoding="utf-8")
+    cause.status = 404
+    cause.reason = "Page not found."
+    cause.headers = "One Two Three"
+    if kwargs.get("host") == "AuthError":
+        raise netwitness_api.AuthenticationError(cause=cause, message="test AuthHeader")
+    if kwargs.get("host") == "HTTPError":
+        cause.body = io.BytesIO(cause.body)
+        raise netwitness_api.HTTPError(response=cause, _message="test HTTPError")
+    return _MockSplunkService()
+
+class _MockSplunkService(MagicMock):
+    """Netwitness service mock."""
+
+    def __init__(self):
+        """Mock method."""
+        super().__init__()
+
+@patch(NETWITNESS_CLI_PATCH)
+def test_netwitness_connect_no_params(netwitness_api):
+    """Check failure with no args."""
+    netwitness_api.connect = cli_connect
+
+    nw_driver = NetwitnessDriver()
+    check.is_true(nw_driver.loaded)
+
     with pytest.raises(MsticpyUserConfigError) as mp_ex:
-        netwitness.connect() ## No arguments provided
+        nw_driver.connect()
+        check.is_false(nw_driver.connected)
     check.is_in("no Netwitness connection parameters", mp_ex.value.args)
-
-def test_netwitness_connect_req_params():
-    netwitness=NetwitnessDriver()
-    with pytest.raises(MsticpyUserConfigError) as mp_ex:
-        netwitness.connect(nwhost="1.1.1.1") ## Missing parameters provided
-    check.is_in("no Netwitness connection parameters", mp_ex.value.args)
-
-@patch("msticpy.data.drivers.netwitness_driver.NetwitnessAPI",NetwitnessAPI)
-def test_netwitness_connection_success():
-    mock_response=NetwitnessAPI
-    #mock_response.login(url="http://mockhost",username="mockuser",password="mockpass")
-    mock_response.login
-    netwitness=NetwitnessDriver()
-    netwitness.connect(nwhost="1.1.1.1",nwuser="username",nwpassword="pass")
-    check.is_true(netwitness.connected)
-
-
-#def test_netwitness_connect_no_params -- Done
-#def test_netwitness_connect_req_params -- Done
-#def test_netwitness_query_success
