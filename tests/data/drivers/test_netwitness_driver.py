02f7f4798a70be6b2ddfd49e50a5e2e7e240efab
m3rcury3 <m3rcury3@outlook.com>
2023-11-13 07:09:09 +0300

    test

--- tests/data/drivers/test_netwitness_driver.py
+++ tests/data/drivers/test_netwitness_driver.py
@@ -76,5 +76,5 @@ def test_netwitness_query_success(netwitness_api):
     # trying to get these before connecting should throw
     with pytest.raises(MsticpyNotConnectedError) as mp_ex:
         netwitness_driver.query("some query")
-        check.is_false(netwitness_driver.connected)
+        check.is_true(netwitness_driver.connected)
     check.is_in("not connected to Netwitness", mp_ex.value.args)
